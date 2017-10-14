434b8bb1a603ef15c6047515df4c27282c5a0c5c
Arthur Silva <aretw0@hotmail.com>
2017-10-14 18:29:41 -0300

    Aparentemente terminei toda a camada de comunicação

---
+++ app/services/api/transactionService.js
@@ -0,0 +1,45 @@
+(function(angular){
+    "use strict";
+    angular.module('ufersavdb')
+        .factory('transactionAPI', transactionAPIFactory);
+
+    transactionAPIFactory.$inject = ["$http","config"];
+    function transactionAPIFactory($http,config){
+        var service = {
+            getAll: _getAll, // feito
+            getCountByCli: _getCountByCli, //feito
+            getCountByBk: _getCountByBk, // feito
+            getCountBySt: _getCountBySt, // feito
+            getAllCount: _getAllCount, // feito
+            getBySt: _getBySt, //feito
+            getByBk: _getByBk, // feito
+            getByCli: _getByCli // feito
+        };
+        return service;
+
+        function _getByCli(id) {
+            return $http.get(config.baseUrl + "/transactions/cli/" + id); // feito
+        }
+        function _getByBk(id) {
+            return $http.get(config.baseUrl + "/transactions/bk/" + id); // feito
+        }
+        function _getBySt(id) {
+            return $http.get(config.baseUrl + "/transactions/st/" + id); // feito
+        }
+        function _getCountByCli(id) {
+            return $http.get(config.baseUrl + "/transactions/amount/cli/"+id); // feito
+        }
+        function _getCountByBk(id) {
+            return $http.get(config.baseUrl + "/transactions/amount/bk/"+id); // feito
+        }
+        function _getCountBySt(id) {
+            return $http.get(config.baseUrl + "/transactions/amount/st/"+id); // feito
+        }
+        function _getAllCount() {
+            return $http.get(config.baseUrl + "/transactions/amount"); // feito
+        }
+        function _getAll() {
+            return $http.get(config.baseUrl + "/transactions"); // feito
+        }
+    }
+})(angular);
